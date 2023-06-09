c99efeae1486986a78ae5d0ac3a148d2af647c9f
shikunsheng <1324281299@qq.com>
2023-06-09 17:40:08 +0800

    验证规则放入config

--- src/ICBCPay.php
+++ src/ICBCPay.php
@@ -14,7 +14,7 @@
 
 namespace Klwlbj\Icbc;
 
-use Faker\Provider\Uuid;
+use Faker\Core\Uuid;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Validator;
 use Klwlbj\Icbc\encryption\IcbcConstants;
@@ -35,21 +35,10 @@ class ICBCPay
     private string $password;
     private string $mer_id;
     private string $store_code;
-    private array $generateRule = [
-        'mer_id'                    => 'required|string|size:15',
-        'out_trade_no'              => 'required|string|size:35',
-        'order_amt'                 => 'required|integer|min:0|max:99999999999999999',
-        'trade_date'                => 'required|date_format:Ymd',
-        'trade_time'                => 'required|date_format:H:i:s',
-        'pay_expire'                => 'required|int|min:0|max:3600',
-        'notify_flag'               => 'required|int|between:0,1',
-        'tporder_create_ip'         => 'required|ip',
 
-        'terminal_info'             => 'required|array',
-        'terminal_info.device_type' => 'required|int',
-        'terminal_info.device_id'   => 'required|string',
-    ];
     private DefaultIcbcClient $client;
+
+    private Uuid $uuidGenerator;
 
     public function __construct($appId = null, $mer_id = null, $store_code = null, $privateKey = null, $icbcPulicKey = null, $signType = null, $encryptKey = null, $encryptType = null, $charset = null, $format = null, $ca = '', $password = null)
     {
@@ -59,12 +48,14 @@ class ICBCPay
         $this->privateKey   = $privateKey ?? config('icbc.privateKey', '');
         $this->icbcPulicKey = $icbcPulicKey ?? config('icbc.icbcPulicKey', '');
         $this->encryptKey   = $encryptKey ?? config('icbc.encryptKey', '');
-        $this->encryptType  = $icbcPulicKey ?? config('icbc.encryptType', '');
-        $this->password     = $icbcPulicKey ?? config('icbc.password', '');
-        $this->signType     = $icbcPulicKey ?? config('icbc.signType', IcbcConstants::$SIGN_TYPE_RSA2);
-        $this->charset      = $icbcPulicKey ?? config('icbc.charset', IcbcConstants::$CHARSET_UTF8);
-        $this->format       = $icbcPulicKey ?? config('icbc.format', IcbcConstants::$FORMAT_JSON);
+        $this->encryptType  = $encryptType ?? config('icbc.encryptType', '');
+        $this->password     = $password ?? config('icbc.password', '');
+        $this->signType     = $signType ?? config('icbc.signType', IcbcConstants::$SIGN_TYPE_RSA2);
+        $this->charset      = $charset ?? config('icbc.charset', IcbcConstants::$CHARSET_UTF8);
+        $this->format       = $format ?? config('icbc.format', IcbcConstants::$FORMAT_JSON);
         $this->ca           = preg_replace("/\s*|\t/", "", $ca) ?? '';
+
+        $this->uuidGenerator = new Uuid();
 
         $this->client = new DefaultIcbcClient($this->appId, $this->privateKey, $this->signType, $this->charset, $this->format, $this->icbcPulicKey, $this->encryptKey, $this->encryptType, $this->ca, $this->password);
     }
@@ -82,7 +73,7 @@ class ICBCPay
      * @throws ValidationException
      */
     public function validateParams(
-        array | Collection $params,
+        array | Collection &$params,
         array $rules = [],
     ) {
         if ($params instanceof Collection) {
@@ -105,60 +96,41 @@ class ICBCPay
     public function generate(array $params = [])
     {
         // 检测参数格式
-        $this->validateParams($params, $this->generateRule);
+        $this->validateParams($params, config('icbc.rules.qrcode.' . __FUNCTION__));
 
-        $request = [
-            "serviceUrl"    => config('icbc.url.qrcode.generate'),
+        $params['mer_id']     = $this->mer_id;
+        $params['store_code'] = $this->store_code;
+        $request              = [
+            "serviceUrl"    => config('icbc.url.qrcode.' . __FUNCTION__),
             "method"        => 'POST',
             "isNeedEncrypt" => false,
-            "biz_content"   => [
-                "mer_id"            => $this->mer_id,
-                "out_trade_no"      => $params['out_trade_no'],
-                "order_amt"         => $params['order_amt'],
-                "trade_date"        => $params['trade_date'],
-                "trade_time"        => $params['trade_time'],
-                "pay_expire"        => $params['pay_expire'],
-                "notify_flag"       => $params['notify_flag'],
-                "terminal_info"     => $params['terminal_info'],
-                "tporder_create_ip" => $params['tporder_create_ip'],
-
-                "store_code"        => $this->store_code,
-            ],
+            "biz_content"   => $params,
         ];
 
-        $msgId = Uuid::uuid();
+        $msgId = $this->uuidGenerator->uuid3();
         $resp  = $this->client->execute($request, $msgId, '');
         return json_decode($resp, true);
     }
 
     /**
      * 二维码退款
-     * @param $reject_no
-     * @param $reject_amt
-     * @param null $cust_id
-     * @param null $out_trade_no
-     * @param null $order_id
-     * @param null $oper_id
+     * @param array $params
      * @return mixed
-     * @throws \Exception
+     * @throws ValidationException
      */
-    public function reject($reject_no, $reject_amt, $cust_id = null, $out_trade_no = null, $order_id = null, $oper_id = null)
+    public function reject(array $params = [])
     {
-        $request = [
-            "serviceUrl"    => config('icbc.url.qrcode.reject', null),
+        // 检测参数格式
+        $this->validateParams($params, config('icbc.rules.qrcode.' . __FUNCTION__));
+
+        $params['mer_id'] = $this->mer_id;
+        $request          = [
+            "serviceUrl"    => config('icbc.url.qrcode.reject' . __FUNCTION__),
             "method"        => 'POST',
             "isNeedEncrypt" => false,
-            "biz_content"   => [
-                "mer_id"       => $this->mer_id,
-                "reject_no"    => $reject_no,
-                "reject_amt"   => $reject_amt,
-                "cust_id"      => $cust_id,
-                "out_trade_no" => $out_trade_no,    //该字段非必输项,out_trade_no和order_id选一项上送即可
-                "order_id"     => $order_id,            //该字段非必输项,out_trade_no和order_id选一项上送即可
-                "oper_id"      => $oper_id,
-            ],
+            "biz_content"   => $params,
         ];
-        $msgId = Uuid::uuid();
+        $msgId = $this->uuidGenerator->uuid3();
         $resp  = $this->client->execute($request, $msgId, '');
         return json_decode($resp, true);
     }
@@ -171,82 +143,42 @@ class ICBCPay
      * @return mixed
      * @throws \Exception
      */
-    public function query($cust_id = null, $out_trade_no = null, $order_id = null)
+    public function query(array $params = [])
     {
-        $request = [
-            "serviceUrl"    => config('icbc.url.qrcode.query', null),
+        // 检测参数格式
+        $this->validateParams($params, config('icbc.rules.qrcode.' . __FUNCTION__));
+
+        $params['mer_id'] = $this->mer_id;
+        $request          = [
+            "serviceUrl"    => config('icbc.url.qrcode.query' . __FUNCTION__),
             "method"        => 'POST',
             "isNeedEncrypt" => false,
-            "biz_content"   => [
-                "mer_id"       => $this->mer_id,
-                "cust_id"      => $cust_id,              //该字段非必输项
-                "out_trade_no" => $out_trade_no,    //该字段非必输项,out_trade_no和order_id选一项上送即可
-                "order_id"     => $order_id,            //该字段非必输项,out_trade_no和order_id选一项上送即可
-            ],
+            "biz_content"   => $params,
         ];
-        $msgId = Uuid::uuid();
+        $msgId = $this->uuidGenerator->uuid3();
         $resp  = $this->client->execute($request, $msgId, '');
         return json_decode($resp, true);
     }
 
     /**
      * 二维码被扫支付
-     * @param $qr_code
-     * @param $out_trade_no
-     * @param $order_amt
-     * @param $trade_date
-     * @param $trade_time
+     * @param array $params
      * @return mixed
-     * @throws \Exception
+     * @throws ValidationException
      */
-    public function pay($qr_code, $out_trade_no, $order_amt, $trade_date, $trade_time)
+    public function pay(array $params = [])
     {
-        $request = [
-            "serviceUrl"    => config('icbc.url.qrcode.pay', null),
-            "method"        => 'POST',
-            "isNeedEncrypt" => false,
-            "biz_content"   => [
-                "qr_code"      => $qr_code,
-                "mer_id"       => $this->mer_id,
-                "out_trade_no" => $out_trade_no,
-                "order_amt"    => $order_amt,
-                "trade_date"   => $trade_date,
-                "trade_time"   => $trade_time,
-            ],
-        ];
-        $msgId = Uuid::uuid();
-        $resp  = $this->client->execute($request, $msgId, '');
-        return json_decode($resp, true);
-    }
+        // 检测参数格式
+        $this->validateParams($params, config('icbc.rules.qrcode.' . __FUNCTION__));
 
-    /**
-     * 二维码冲正
-     * @param $out_trade_no
-     * @param null $cust_id
-     * @param null $order_id
-     * @param null $reject_no
-     * @param null $reject_amt
-     * @param null $oper_id
-     * @return mixed
-     * @throws \Exception
-     */
-    public function reverse(string $out_trade_no, $cust_id = null, $order_id = null, $reject_no = null, $reject_amt = null, $oper_id = null)
-    {
-        $request = [
-            "serviceUrl"    => config('icbc.url.qrcode.reverse', null),
+        $params['mer_id'] = $this->mer_id;
+        $request          = [
+            "serviceUrl"    => config('icbc.url.qrcode.' . __FUNCTION__),
             "method"        => 'POST',
             "isNeedEncrypt" => false,
-            "biz_content"   => [
-                "mer_id"       => $this->mer_id,
-                "out_trade_no" => $out_trade_no,
-                "cust_id"      => $cust_id,              //该字段非必输项
-                "order_id"     => $order_id,            //该字段非必输项
-                "reject_no"    => $reject_no,          //该字段非必输项
-                "reject_amt"   => $reject_amt,        //该字段非必输项
-                "oper_id"      => $oper_id,               //该字段非必输项
-            ],
+            "biz_content"   => $params,
         ];
-        $msgId = Uuid::uuid();
+        $msgId = $this->uuidGenerator->uuid3();
         $resp  = $this->client->execute($request, $msgId, '');
         return json_decode($resp, true);
     }
@@ -274,7 +206,7 @@ class ICBCPay
                 "cust_id"      => $cust_id,
             ],
         ];
-        $msgId = Uuid::uuid();
+        $msgId = $this->uuidGenerator->uuid3();
         $resp  = $this->client->execute($request, $msgId, '');
         return json_decode($resp, true);
     }
--- src/config/icbc.php
+++ src/config/icbc.php
@@ -36,12 +36,47 @@ return [
     // 将需要用到的接口及对应地址放在这里 小写驼峰=>url
     'url'          => [
         'qrcode' => [
-            'generate'    => $domain . '/api/cardbusiness/qrcode/qrgenerate/V3',
-            'pay'         => $domain . '/api/qrcode/V2/pay',
-            'query'       => $domain . '/api/qrcode/V2/query',
-            'reject'      => $domain . '/api/qrcode/V2/reject',
-            'reverse'     => $domain . '/api/qrcode/V2/reverse',
-            'rejectQuery' => $domain . '/api/qrcode/reject/query/V3',
+            'generate'  => $domain . '/api/cardbusiness/qrcode/qrgenerate/V3',
+            'pay'       => $domain . '/api/mybank/pay/qrcode/scanned/pay/V5',
+            'paystatus' => $domain . '/api/mybank/pay/qrcode/scanned/paystatus/V4',
+            'reject'    => $domain . '/api/mybank/pay/qrcode/scanned/return/V2',
+        ],
+    ],
+    'rules'        => [
+        'qrcode' => [
+            'generate'  => [
+                'out_trade_no'              => 'required|string|size:35',
+                'order_amt'                 => 'required|integer|min:0|max:99999999999999999',
+                'trade_date'                => 'required|date_format:Ymd',
+                'trade_time'                => 'required|date_format:H:i:s',
+                'pay_expire'                => 'required|int|min:0|max:3600',
+                'notify_flag'               => 'required|int|between:0,1',
+                'tporder_create_ip'         => 'required|ip',
+
+                'terminal_info'             => 'required|array',
+                'terminal_info.device_type' => 'required|int',
+                'terminal_info.device_id'   => 'required|string',
+            ],
+            'pay'       => [
+                'qr_code'      => 'required|string|size:19',
+                'out_trade_no' => 'required|string|size:35',
+                // 'trade_date' => 'required|string|size:19',// 暂不使用
+                // 'trade_time' => 'required|string|size:19',// 暂不使用
+                'attach'       => 'string',
+                'order_amt'    => 'required|int|max:99999999999999999',
+            ],
+            'reject'    => [
+                'out_trade_no' => 'required|string|size:35',
+                "reject_no"    => "'required_without:order_id',",
+                "order_id"     => "'required_without:reject_no',",
+                'reject_amt'   => 'required|int|max:99999999999999999',
+                "oper_id"      => 'required|string|size:3',
+            ],
+            'paystatus' => [
+                'out_trade_no' => 'required_without:order_id|string|size:35',
+                "order_id"     => 'required_without:out_trade_no',
+                'deal_flag'    => 'int|in:0,1',
+            ],
         ],
     ],
 ];
